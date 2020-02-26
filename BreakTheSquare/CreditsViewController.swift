a68b938eebf4ecc966124ab94bea1edc5c979091
reycritt <reycrittenden@gmail.com>
2020-02-26 11:00:56 -0600

    Some storyboard views

---
+++ BreakTheSquare/CreditsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreditsViewController.swift
+//  BreakTheSquare
+//
+//  Created by Socstudent on 2/26/20.
+//  Copyright © 2020 royalty. All rights reserved.
+//
+
+import UIKit
+
+class CreditsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
