c07baccb237586ac4c00e9ac42a5c38be98e7708
reycritt <reycrittenden@gmail.com>
2020-02-26 15:45:16 -0600

    Workings of the clicker

--- BreakTheSquare/GameViewController.swift
+++ BreakTheSquare/GameViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    
+    @IBOutlet weak var squareHealthLabel: UILabel!
+    //Might have a problem with setting the images of the square
+    
+    var squareMaxHealth = 100
+    var squareCurrentHealth = 100
+    var coins = 0//In-game currency
+    var coinWorth = 5
+    var coinReward = 20
+    var playerDamage = 5
+    var level = 0//Counter that changes difficulty and various game aspects
+    //var coinMultiplier = ["first": 4, "second": 3]//This is an experimental dictionary of multiplier values
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +28,38 @@ class GameViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func tapSquare(_ sender: Any) {
+        coins += coinWorth
+        print(coins)
+        print(squareCurrentHealth)
+        squareCurrentHealth -= playerDamage
+        squareHealthLabel.text = "\(squareCurrentHealth)/\(squareMaxHealth)"
+        
+        squareHealthChecker(squareHealth: squareCurrentHealth)
+    }
+    
+    /*
+     Temporary button to return to main menu; will engage a popup/view that pauses the game and returns player to menu.
+     */
+    @IBAction func goToMenu(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func squareHealthChecker (squareHealth: Int) {//Could be renamed to healthChecker and take in health of all enemies
+        
+        //Insert an if statement to change sqaure image based on remaining health
+        if squareHealth <= 0 {
+            coins += coinReward
+            level += 1
+            levelIncrease()
+            //Call a function that does a short animation involving what's underneath the square
+        }
+    }
+    
+    func levelIncrease () {
+        squareMaxHealth = Int(Double(squareMaxHealth) + (Double(squareMaxHealth) * 0.25))//0.25 is a temporary increase
+        squareCurrentHealth = squareMaxHealth
+    }
 
     /*
     // MARK: - Navigation
