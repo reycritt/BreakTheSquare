564d5008c33816296b1036035a6f0ca80e123111
Rey <reycrittenden@yahoo.com>
2020-02-26 00:28:29 -0600

    Initial Commit

---
+++ BreakTheSquare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BreakTheSquare
+//
+//  Created by cory on 2/26/20.
+//  Copyright © 2020 royalty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
